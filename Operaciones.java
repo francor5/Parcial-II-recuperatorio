be058d969bdc1bf2df29fd565b17e918f6b2a9fe
Franco Rojas <89144104+francor5@users.noreply.github.com>
2021-11-25 12:34:54 -0300

    Add files via upload

---
+++ Operaciones.java
@@ -0,0 +1,17 @@
+package recuperatoriolabII;
+
+public class Operaciones {
+    private  int numeroentero;
+
+    public Operaciones(int numeroentero) {
+        this.numeroentero = numeroentero;
+    }
+
+    public int getNumeroentero() {
+        return numeroentero;
+    }
+
+    public void setNumeroentero(int numeroentero) {
+        this.numeroentero = numeroentero;
+    }
+}
